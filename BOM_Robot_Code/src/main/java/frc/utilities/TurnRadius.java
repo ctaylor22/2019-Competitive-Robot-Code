df632f56406ac75319a9de85bae61d43464d88d1
Conner Taylor <conner.taylor8@gmail.com>
2019-04-17 15:48:44 -0700

    Revert "Merge branch 'Limelight-Pathfinding'"
    This reverts commit b637fd08f38791aeda67552ea71fe5ec6e4d2faa, reversing
    changes made to 1f38af48b5fd84a53fa9e4a99b2c6d8560f8f944.

--- BOM_Robot_Code/src/main/java/frc/utilities/TurnRadius.java
+++ BOM_Robot_Code/src/main/java/frc/utilities/TurnRadius.java
@@ -18,15 +18,15 @@ public class TurnRadius {
       final double time = main_dist/ips;
       double[] radiuses = {0, 0, 0};
       if (direction == 1) {
-          radiuses[0] = calculateTurnDistance(turnradius-wheeloffset, radians);
-          radiuses[1] = calculateTurnDistance(turnradius+wheeloffset, radians);
-          radiuses[2] = feetToEncoder(turnradius-wheeloffset, wheelradius);
-          radiuses[3] = feetToEncoder(turnradius+wheeloffset, wheelradius);
+          radiuses[0] = calculateTurnDistance(wheelradius-wheeloffset, radians);
+          radiuses[1] = calculateTurnDistance(wheelradius+wheeloffset, radians);
+          radiuses[2] = feetToEncoder(wheelradius-wheeloffset, wheelradius);
+          radiuses[3] = feetToEncoder(wheelradius+wheeloffset, wheelradius);
       } else if (direction == 2) {
-          radiuses[0] = calculateTurnDistance(turnradius+wheeloffset, radians);
-          radiuses[1] = calculateTurnDistance(turnradius-wheeloffset, radians);
-          radiuses[2] = feetToEncoder(turnradius+wheeloffset, wheelradius);
-          radiuses[3] = feetToEncoder(turnradius-wheeloffset, wheelradius);
+          radiuses[0] = calculateTurnDistance(wheelradius+wheeloffset, radians);
+          radiuses[1] = calculateTurnDistance(wheelradius-wheeloffset, radians);
+          radiuses[2] = feetToEncoder(wheelradius+wheeloffset, wheelradius);
+          radiuses[3] = feetToEncoder(wheelradius-wheeloffset, wheelradius);
       }
       double[] radius_speeds = {radiuses[0]/time, radiuses[1]/time};
       radius_speeds[0] = ipsToRpm(radius_speeds[0], wheelradius);
@@ -34,28 +34,6 @@ public class TurnRadius {
       return radius_speeds;
   }
 
-  public static double turnRadiusRatio(double turnradius, double wheeloffset, double direction) {
-    /* Returns how many spins the right motor needs to do for each spin of the left motor
-     * turnradius: the radius of the circle to turn about, currently tangent to the robot
-     * wheeloffset: the distance of the wheels from the center of the robot
-     * direction: wether the robot wants to turn right or left
-     */
-    //Converts arguments to inches
-    double ratio;
-    double rightradius;
-    double leftradius;
-    turnradius *= 12;
-    wheeloffset *= 12;
-    if (direction == 1) {
-        rightradius = turnradius-wheeloffset;
-        leftradius = turnradius+wheeloffset;
-    } else {
-        rightradius = turnradius+wheeloffset;
-        leftradius = turnradius-wheeloffset;
-    }
-    ratio = rightradius/leftradius;
-    return ratio;
-  }
   static double feetToEncoder(double dist, double wheelradius) {
       return (dist/(2*wheelradius*pi))*Constants.ticksPerRotation;
   }
